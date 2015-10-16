9159d406f06e66286358b667f8019514ac766521
Bartosz Sądel <bsadel69@gmail.com>
2015-10-16 23:30:46 +0200

    saving in db

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -4,12 +4,21 @@ import config
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+from pymongo import MongoClient
+import json
+
+
+client = MongoClient('localhost', 27017)
+db = client['twitter_db']
+collection = db['twitter_collection']
 
 
 class StdOutListener(StreamListener):
 
     def on_data(self, raw_data):
         print raw_data
+        json_tweet = json.loads(raw_data)
+        collection.insert(json_tweet)
         return True
 
     def on_error(self, status_code):
